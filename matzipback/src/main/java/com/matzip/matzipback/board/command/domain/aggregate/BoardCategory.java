1423e0641e057b7e0a7843000302260a39bcd901
JIYOUNG-22 <dyong0922@naver.com>
2024-10-18 00:55:59 +0900

    feat: 게시판 카테고리 등록을 위한 Entity 수정

--- matzipback/src/main/java/com/matzip/matzipback/board/command/domain/aggregate/BoardCategory.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/domain/aggregate/BoardCategory.java
@@ -1,16 +1,18 @@
 package com.matzip.matzipback.board.command.domain.aggregate;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "board_category")
+@Getter
 @NoArgsConstructor
 public class BoardCategory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int boardCategorySeq;
+    private Long boardCategorySeq;
     private String boardCategoryName;
 
 }
