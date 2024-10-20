d5dfcc14c5af760d3eea0d622a3da99f8c164a67
WhooGeek <rlarjswn0303@naver.com>
2024-10-20 16:00:30 +0900

    feat: #43 리스트 전체 조회 기능 구현 ListSearchDTO 추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/query/dto/ListSearchDTO.java
@@ -0,0 +1,16 @@
+package com.matzip.matzipback.matzipList.query.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ListSearchDTO {
+    private long listUserSeq;
+    private String userNickname;
+    private String listTitle;
+    private String listContent;
+    private int listLike;
+}
