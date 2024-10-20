5b6db532e338fc38fe6c0c1d42e919899f3843f2
WhooGeek <rlarjswn0303@naver.com>
2024-10-20 16:00:17 +0900

    feat: #43 리스트 전체 조회 기능 구현 ListQueryService 코드 추가

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/query/service/ListQueryService.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/query/service/ListQueryService.java
@@ -15,6 +15,26 @@ public class ListQueryService {
 
     private final ListQueryMapper listQueryMapper;
 
+    // 리스트 전체 조회
+    @Transactional
+    public ListAllResponse getAllList(Integer page, Integer size){
+
+        // offset: 몇번째 페이지를 펼지 설정
+        int offset = (page - 1) * size;
+
+        List<ListSearchDTO> listAll = listQueryMapper.getListAll(offset, size);
+
+        Long totalLists = listQueryMapper.countListsByList();
+
+        return ListAllResponse.builder()
+                .listSearchAllDTOs(listAll)
+                .currentPage(page)
+                .totalPages((int) Math.ceil((double)totalLists / size))
+                .totalLists(totalLists)
+                .build();
+    }
+
+    // 유저 본인의 리스트 박스 조회
     @Transactional
     public ListBoxResponse getListBox(Integer page, Integer size, Long listUserSeq) {
 
@@ -26,18 +46,21 @@ public class ListQueryService {
         Long totalLists = listQueryMapper.countListsByListUserSeq(listUserSeq);
 
         return ListBoxResponse.builder()
-                        .
-                listSearchUserDTOs(ListBox)
+
+                .listSearchUserDTOs(ListBox)
                 .currentPage(page)
                 .totalPages((int) Math.ceil((double)totalLists / size))
                 .totalLists(totalLists)
                 .build();
     }
 
-
+    // 리스트 상세 조회
+//    @Transactional
     public List<ListContentDTO> getListContests(Long listSeq) {
         return listQueryMapper.getListContests(listSeq);
     }
+
+    // 다른 유저의 리스트
     @Transactional(readOnly = true)
     public ListBoxResponse getUserListBox(Integer page, Integer size, Long listUserSeq) {
 
