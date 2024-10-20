d1dc1faa2fd3cacd2c03177441e429b3825cb918
whoogeek <rlarjswn0303@naver.com>
2024-10-20 14:13:32 +0900

    chore: 유저 본인 리스트 조회 페이징 컨트롤러 수정 #36

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/query/controller/ListQueryController.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/query/controller/ListQueryController.java
@@ -2,6 +2,8 @@ package com.matzip.matzipback.matzipList.query.controller;
 
 import com.matzip.matzipback.common.util.CustomUserUtils;
 
+import com.matzip.matzipback.exception.ErrorCode;
+import com.matzip.matzipback.exception.RestApiException;
 import com.matzip.matzipback.matzipList.query.dto.ListBoxResponse;
 import com.matzip.matzipback.matzipList.query.dto.ListContentDTO;
 
@@ -29,10 +31,23 @@ public class ListQueryController {
     // 유저 본인의 리스트 서랍 조회(모든 리스트 상태 조회)
     @GetMapping("/listbox")
     @Operation(summary = "본인 리스트 서랍 조회", description = "본인의 리스트 서랍을 조회한다.")
-    public ResponseEntity<List<ListSearchAllDTO>> getListBox() {
-        //URL에서 유저 시퀀스 받지 않고 로그인한 사용자의 유저 시퀀스를 받아오는 것으로 구현
+    public ResponseEntity<SuccessSearchResMessage<?>> getListBox(
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "10") Integer size
+    ) {
         Long listUserSeq = CustomUserUtils.getCurrentUserSeq();
-        return ResponseEntity.ok().body(listQueryService.getListBox(listUserSeq));
+
+        if (listUserSeq != null){
+            //URL에서 유저 시퀀스 받지 않고 로그인한 사용자의 유저 시퀀스를 받아오는 것으로 구현
+            ListBoxResponse response = listQueryService.getListBox(page, size, listUserSeq);
+
+            return ResponseEntity.ok()
+                    .body(new SuccessSearchResMessage<>(
+                            SuccessCode.BASIC_GET_SUCCESS
+                            , response
+                    ));
+        }else{
+            throw new RestApiException(ErrorCode.UNAUTHORIZED_REQUEST);}
     }
 
     // 다른 유저의 리스트 서랍 조회
@@ -55,8 +70,15 @@ public class ListQueryController {
     // 리스트 상세 조회
     @GetMapping("/listbox/list/{listSeq}")
     @Operation(summary = "리스트 상세 조회", description = "공개된 리스트를 상세 조회한다.")
-    public ResponseEntity<List<ListContentDTO>> getListContents(@PathVariable("listSeq") Long listSeq) {
-        return ResponseEntity.ok().body(listQueryService.getListContests(listSeq));
+    public ResponseEntity<SuccessSearchResMessage<?>> getListContents(@PathVariable("listSeq") Long listSeq) {
+//        return ResponseEntity.ok().body(listQueryService.getListContests(listSeq));
+        List<ListContentDTO> response = listQueryService.getListContests(listSeq);
+
+        return ResponseEntity.ok()
+                .body(new SuccessSearchResMessage<>(
+                        SuccessCode.BASIC_GET_SUCCESS
+                        , response
+                ));
     }
 
 }
