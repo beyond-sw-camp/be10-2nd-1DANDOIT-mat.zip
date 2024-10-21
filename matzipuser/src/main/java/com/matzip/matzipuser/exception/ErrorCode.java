fcbefe2654dda4eb0e83118bb522c05c873ad30b
catnine11 <catnine11@gmail.com>
2024-10-21 17:53:26 +0900

    chore: 관련 에러코드 추가

--- matzipuser/src/main/java/com/matzip/matzipuser/exception/ErrorCode.java
+++ matzipuser/src/main/java/com/matzip/matzipuser/exception/ErrorCode.java
@@ -57,6 +57,9 @@ public enum ErrorCode {
     /* 닉네임 중복 */
     NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 사용 중인 닉네임입니다. 다른 닉네임을 선택해주세요."),
 
+    /* 동일한 닉네임으로 수정 */
+    NOT_UPDATED_NICKNAME(HttpStatus.CONFLICT, "현재 닉네임과 동일한 닉네임으로는 변경할 수 없습니다."),
+
     /* 휴대폰번호 중복 */
     USER_PHONE_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 사용 중인 번호입니다."),
 
