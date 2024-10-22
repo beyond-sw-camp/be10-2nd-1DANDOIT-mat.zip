4731c94a482172690ed75953f2712078bb0e5fdf
catnine11 <catnine11@gmail.com>
2024-10-22 16:14:50 +0900

    feat: 만료된 토큰을 삭제하는 jpql

--- matzipuser/src/main/java/com/matzip/matzipuser/users/command/infrastructure/repository/UsersInfraRepository.java
+++ matzipuser/src/main/java/com/matzip/matzipuser/users/command/infrastructure/repository/UsersInfraRepository.java
@@ -3,6 +3,16 @@ package com.matzip.matzipuser.users.command.infrastructure.repository;
 import com.matzip.matzipuser.users.command.domain.aggregate.Users;
 import com.matzip.matzipuser.users.command.domain.repository.UsersDomainRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 public interface UsersInfraRepository extends JpaRepository<Users, Long>, UsersDomainRepository {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Users u SET u.pwResetToken = null, u.pwTokenDueTime = null WHERE u.pwTokenDueTime < :currentTime")
+    void clearExpiredTokens(LocalDateTime currentTime);
 }
