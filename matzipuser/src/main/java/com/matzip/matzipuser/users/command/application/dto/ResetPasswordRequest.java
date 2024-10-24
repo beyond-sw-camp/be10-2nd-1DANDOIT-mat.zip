2f6fb8a87ba5987908b36f1f73aaf82588f4a3b3
catnine11 <catnine11@gmail.com>
2024-10-24 18:37:11 +0900

    chore: 비밀번호 변경 유효값(정규화패턴) 추가

--- matzipuser/src/main/java/com/matzip/matzipuser/users/command/application/dto/ResetPasswordRequest.java
+++ matzipuser/src/main/java/com/matzip/matzipuser/users/command/application/dto/ResetPasswordRequest.java
@@ -1,5 +1,6 @@
 package com.matzip.matzipuser.users.command.application.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +10,8 @@ import java.time.LocalDateTime;
 @Setter
 public class ResetPasswordRequest {
 
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*.,?])[A-Za-z\\d!@#$%^&*.,?]{8,}$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 포함한 8자 이상이어야 합니다.")
     private String userPassword;
+
 }
