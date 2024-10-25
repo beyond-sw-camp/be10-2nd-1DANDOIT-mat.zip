ddfbe16ed2b63a5d21c6429d11f9ff34dc8c4fda
catnine11 <catnine11@gmail.com>
2024-10-25 21:41:39 +0900

    chore: 유효값검증 수정

--- matzipuser/src/main/java/com/matzip/matzipuser/users/command/application/dto/EmailSendRequest.java
+++ matzipuser/src/main/java/com/matzip/matzipuser/users/command/application/dto/EmailSendRequest.java
@@ -1,11 +1,16 @@
 package com.matzip.matzipuser.users.command.application.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class EmailSendRequest {
+    @NotBlank(message = "이메일은 필수 입력사항입니다.")
+    @Email(message = "유효한 이메일 주소를 입력해주세요.")
     private String userEmail;
+    @NotBlank(message = "이름은 필수 입력 사항입니다.")
     private String userName;
 }
