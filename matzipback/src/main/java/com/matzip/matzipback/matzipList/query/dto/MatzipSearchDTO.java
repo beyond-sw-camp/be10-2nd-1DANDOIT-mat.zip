2cfeccf94a8b8300b889131c33d92e9e32ab7179
whoogeek <rlarjswn0303@naver.com>
2024-10-26 16:59:54 +0900

    fix: MatzipSearchDTO 수정

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/query/dto/MatzipSearchDTO.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/query/dto/MatzipSearchDTO.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MatzipSearchDTO {
+    private String restaurantSeq;
     private String restaurantTitle;
     private String restaurantAddress;
     private String restaurantPhone;
